0e86d1cc0770ddf7a8a658dba8705365e4e05d0b
David <kansas@inkwhiskey.com>
2016-07-27 14:34:07 -0600

    twerking pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 
+var gulp = require('gulp')
+
 // SASS
 // ============================================================
 
-var gulp = require('gulp'),
-		sass = require('gulp-sass');
+var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
     gulp.src('src/sass/**/*.scss')
@@ -14,30 +15,36 @@ gulp.task('sass', function() {
 // PUG
 // ============================================================
 
-var gulp = require('gulp'),
-    pug = require('gulp-jade');
+var pug = require('gulp-pug');
+ 
+// gulp.task('pug', function buildHTML() {
+//   return gulp.src('./views/**/*.pug')
+//   .pipe(pug({
+//     pretty: true
+//   }))
+//   .pipe(gulp.dest('./public'))
+// });
 
-// run this task by typing in gulp pug in CLI
 gulp.task('pug', function() {
-    return gulp.src('views/**/*.pug')
-        .pipe(pug()) // pipe to pug plugin
-        .pipe(gulp.dest('public/'));
+  gulp.src('./views/**/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./public'))
 });
 
 // WATCH
 // ============================================================
 
-gulp.task('watch', function () {
-  gulp.watch(['./views/*.pug'], ['pug']),
-  gulp.watch(['./public/*.html'], ['html']),
+gulp.task('watch', function() {
+  gulp.watch(['./views/**/*.pug'], ['pug']),
   gulp.watch(['./src/sass/**/*.scss'],['sass']);
 });
 
 // LIVE RELOAD
 // ============================================================
  
-var gulp = require('gulp'),
-  connect = require('gulp-connect');
+var connect = require('gulp-connect');
 
 gulp.task('connect', function() {
   connect.server({
